e5db564bc42df604cffc1fb8b336117cc79cd5dc
THISARA RATHNAYAKA <76846289+thisaraanjana@users.noreply.github.com>
2024-10-24 11:56:02 +0530

    update

--- foodapp/src/components/FoodDetails.jsx
+++ foodapp/src/components/FoodDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 export default function FoodDetails({ foodId }) {
   const [food, setFood] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
   const API_KEY = "a0a06182dc424d779dabca5adb164c79";
   useEffect(() => {
@@ -10,18 +11,36 @@ export default function FoodDetails({ foodId }) {
       console.log(data);
       console.log(foodId);
       setFood(data);
+      setIsLoading(false);
     }
     fetchFood();
-  }, [foodId]);//identify fod is change and code is do change that details
-  return <div>
+  }, [foodId]); //identify fod is change and code is do change that details
+  return (
     <div>
+      <div>
         <h1>{food.title}</h1>
 
         <img src={food.image} alt="" />
       </div>
-      <span>
-        <strong>{food.readyInMinutes} Minutes</strong>
-      </span>
-      <span>{food.vegetarian ? "vegetarian":"Non-vegetarian"}</span>
-  </div>;
+      <div>
+        <span>
+          <strong>{food.readyInMinutes} Minutes</strong>
+        </span>
+        <span> Serves{food.servings} people</span>
+        <span>{food.vegetarian ? " vegetarian" : " Non-vegetarian"}</span>
+        <span>{food.vegan ? "  vegan" : "  non-vegan"}</span>
+      </div>
+      <div>
+        <span>${food.pricePerServing}</span>
+      </div>
+      <div>
+        <h3>Instruction</h3>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          food.analyzedInstructions[0].steps.map((step) => <li>{step.step}</li>)
+        )}
+      </div>
+    </div>
+  );
 }
